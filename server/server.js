e3a145bfe36cb078f4be0c2eef36739b82ed0f9c
Dionne Angeles <jr8dionne@yahoo.com>
2023-02-23 17:05:26 -0800

    added schemas folder

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,7 +16,24 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => {
+    // get the user token from the headers
+    const token = req.headers.authorization || '';
+
+    // try to retrieve a user with the token
+    const user = getUser(token);
+
+    // add the db models and the user to the context
+    return { user };
+  },
+});
+
+server.start().then(() => {
+  server.applyMiddleware({ app });
+});
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
