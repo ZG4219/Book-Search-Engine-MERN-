62457f94fe7241dbc3759a9528bd471113103eab
Dionne Angeles <jr8dionne@yahoo.com>
2023-03-03 00:30:55 -0800

    modified index.js

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,4 +1,25 @@
-const typeDefs = require('./typeDefs');
-const resolvers = require('./resolvers');
+const { gql } = require('apollo-server-express');
 
-module.exports = { typeDefs, resolvers };
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    books: () => {
+      // logic to fetch books from a data source
+    },
+  },
+};
+module.exports = {
+    typeDefs,
+    resolvers,
+  };
+  
