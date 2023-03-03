0f291fc002521366de926e183f0b7adc86a8ce90
Dionne Angeles <jr8dionne@yahoo.com>
2023-03-03 00:32:39 -0800

    modified auth.js

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,49 +1,18 @@
-// use this to decode a token and get the user's information out of it
-import decode from 'jwt-decode';
+const jwt = require('jsonwebtoken');
+const { AuthenticationError } = require('apollo-server-express');
 
-// create a new class to instantiate for a user
-class AuthService {
-  // get user data
-  getProfile() {
-    return decode(this.getToken());
-  }
-
-  // check if user's logged in
-  loggedIn() {
-    // Checks if there is a saved token and it's still valid
-    const token = this.getToken();
-    return !!token && !this.isTokenExpired(token); // handwaiving here
-  }
-
-  // check if token is expired
-  isTokenExpired(token) {
+const authMiddleware = ({ req }) => {
+  let token = req.headers.authorization;
+  if (token) {
+    token = token.split(' ')[1];
     try {
-      const decoded = decode(token);
-      if (decoded.exp < Date.now() / 1000) {
-        return true;
-      } else return false;
+      const user = jwt.verify(token, process.env.JWT_SECRET);
+      return { user };
     } catch (err) {
-      return false;
+      throw new AuthenticationError('Invalid/Expired token');
     }
   }
+  throw new AuthenticationError('Authorization header must be provided');
+};
 
-  getToken() {
-    // Retrieves the user token from localStorage
-    return localStorage.getItem('id_token');
-  }
-
-  login(idToken) {
-    // Saves user token to localStorage
-    localStorage.setItem('id_token', idToken);
-    window.location.assign('/');
-  }
-
-  logout() {
-    // Clear user token and profile data from localStorage
-    localStorage.removeItem('id_token');
-    // this will reload the page and reset the state of the application
-    window.location.assign('/');
-  }
-}
-
-export default new AuthService();
+module.exports = authMiddleware;
